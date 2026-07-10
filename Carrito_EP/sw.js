5f32127ce4521765a8b38b12461095f2cf66a22d
JoseDGarciaP <josexd210711@gmail.com>
2026-07-09 21:29:29 -0500

    Catalogo V17

--- Carrito_EP/sw.js
+++ Carrito_EP/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'analisis-mercado-v7.4.2';
+const CACHE = 'analisis-mercado-v7.4.3';
 // Los JSON de beneficiarios (./json_ups/{codigo}.json) NO se pre-cachean
 // aquí porque son demasiados; se cachean automáticamente al consultarse.
 const ASSETS = ['./', './index.html', './productos.js', './manifest.json', './icon.png'];
